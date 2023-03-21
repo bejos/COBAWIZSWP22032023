6c42af2ce3de25e8915663ab591813ece8363c1f
Mlbb Fan <77885074+retjehnetwork@users.noreply.github.com>
2023-03-22 06:52:01 +0700

    A

--- src/constants/eggPriceContracts.ts
+++
@@ -1,7 +0,0 @@
-const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0x81e032c97a5cbbf06851412e2b1adcb2b51de9d1',
-  busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x042284533c81ed74e041a3697aa547bf1c836e0a'
-}
-
-export default priceContracts
